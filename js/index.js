a550a60f8906188d79ff18202dcbbf95f5c9a203
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-02-11 13:18:31 -0800

    initial commit, first 5 events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-// Your code goes here
+//nav -mouseenter/leave
+document.querySelectorAll('.nav-link').forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        el.style.color = 'red';
+        el.style.transform = 'scale(1.3)';
+    });
+});
+
+document.querySelectorAll('.nav-link').forEach(el => {
+    el.addEventListener('mouseleave', () => {
+        el.style.color = 'black';
+        el.style.transform = 'scale(1)';
+    });
+});
+
+//header image -dbl click
+let changeImg = document.querySelector('#bus');
+changeImg.addEventListener('dblclick', () => {
+    changeImg.src = 'img/bus2.jpg';
+});
+
+
+//logo -click
+let logoColor = document.querySelector('.logo-heading');
+logoColor.addEventListener('click', () => {
+    logoColor.style.color = '#FF00ff'
+});
+
+//paragraphs -mouseenter/leave
+document.querySelectorAll('div p').forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        el.style.background = '#AFEEEE';
+    });
+});
+document.querySelectorAll('div p').forEach(el => {
+    el.addEventListener('mouseleave', () => {
+        el.style.background = 'transparent';
+    });
+});
+
+//window -scroll
+window.addEventListener('scroll', () => {
+    const body = document.querySelector('body');
+    body.style.background = '#FFB6C1';
+});
+
+//window -load
+window.addEventListener('load', () => {
+    alert('fully loaded :p');
+})
